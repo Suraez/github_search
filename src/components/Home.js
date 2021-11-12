b602226ec99eb0df7d539feb008b29f61e0e23fd
Suraez <oberais50@gmail.com>
2021-11-12 13:26:30 +0545

    successfully setup routing

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Footer from "./partials/Footer";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 style={{ color: "white" }}>Home</h1>
+      <Footer />
+    </div>
+  );
+}
