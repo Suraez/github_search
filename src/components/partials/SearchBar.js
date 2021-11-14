9230b2b92954fc1cb12e6cf51665d3245aca1a6e
Suraez <oberais50@gmail.com>
2021-11-14 15:48:18 +0545

    setup redirectin after getting results

--- src/components/partials/SearchBar.js
+++ src/components/partials/SearchBar.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import axios from "axios";
+import { Navigate } from "react-router";
 
 export default function SearchBar(props) {
   const style = props.firstPage ? "d-flex justify-content-center" : "d-flex";
@@ -8,6 +9,7 @@ export default function SearchBar(props) {
   const style3 = props.firstPage ? "mb-3" : "mr-3";
 
   const [queryString, setqueryString] = useState("");
+  const [repoList, setrepoList] = useState([]);
 
   const onSubmitHandler = (e, query) => {
     e.preventDefault();
@@ -18,6 +20,7 @@ export default function SearchBar(props) {
       )
       .then((response) => {
         console.log(response.data);
+        setrepoList(response.data.data);
       })
       .catch((err) => {
         console.log(err);
@@ -25,29 +28,32 @@ export default function SearchBar(props) {
   };
 
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-12">
-          <Form
-            className={style2}
-            onSubmit={(e) => onSubmitHandler(e, queryString)}
-          >
-            <Form.Group className={style3} controlId="formBasicEmail">
-              <Form.Control
-                type="text"
-                placeholder="Enter the name of repo"
-                onChange={(e) => setqueryString(e.target.value)}
-              />
-            </Form.Group>
+    <>
+      {repoList.length !== 0 ? <Navigate to="/search-results" /> : null}
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <Form
+              className={style2}
+              onSubmit={(e) => onSubmitHandler(e, queryString)}
+            >
+              <Form.Group className={style3} controlId="formBasicEmail">
+                <Form.Control
+                  type="text"
+                  placeholder="Enter the name of repo"
+                  onChange={(e) => setqueryString(e.target.value)}
+                />
+              </Form.Group>
 
-            <div className={style}>
-              <Button variant="primary" type="submit">
-                Search
-              </Button>
-            </div>
-          </Form>
+              <div className={style}>
+                <Button variant="primary" type="submit">
+                  Search
+                </Button>
+              </div>
+            </Form>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
